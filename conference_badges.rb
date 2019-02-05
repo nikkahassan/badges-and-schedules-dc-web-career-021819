dfef028df27008bb24da2886bac56b81f624f0e3
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-05 15:53:30 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,15 +1,15 @@
-def badge_maker(speakers)
+def badge_maker(name)
   "Hello, my name is #{speakers}."
 end
 
-def batch_badge_creator(speakers)
-  speakers.collect do |speakers|
-    badge_maker(speakers)
+def batch_badge_creator(name)
+  name.collect do |name|
+    badge_maker(name)
   end
 end
 
-def assign_rooms(speakers)
-  speakers.collect {|speakers| speakers + index }
-    "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
-  end
+def assign_rooms(name)
+	array1.each_with_index.map do |name, index| 
+		"Hello #{name}! You'll be assigned to room #{index + 1}"
+	end
 end
