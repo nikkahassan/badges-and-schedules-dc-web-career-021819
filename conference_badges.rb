453b211dff1d88a51cf42de40b64724470a3f87b
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-05 15:59:42 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,8 +1,9 @@
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def batch_badge_creator(array)
   array.map do |name|
     "Hello, my name is #{name}."
