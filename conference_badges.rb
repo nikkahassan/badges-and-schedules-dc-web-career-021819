c31c4bc9b8a3c404633351a42c1e5c441d61d397
Nikka Hassan <nikka.hassan@gmail.com>
2019-02-05 15:50:59 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,7 +9,7 @@ def batch_badge_creator(speakers)
 end
 
 def assign_rooms(speakers)
-  speakers.collect.each_with_index do |speaker, index|
+  speakers.collect {|x| x + "!" }
     "Hello, #{speaker}! You'll be assigned to room #{index+1}!"
   end
 end
